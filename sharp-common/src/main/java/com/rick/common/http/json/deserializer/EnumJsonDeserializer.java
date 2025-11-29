8595eae4436f331bc6ce6c7a3e92cf423c2cd69d
rick <jkxyx205@163.com>
2025-11-29 20:27:48 +0800

    refactor: EnumJsonDeserializer 已经被 EnumCustomizeDeserializer 替换，这个类在多层结构下，枚举会出现问题

--- sharp-common/src/main/java/com/rick/common/http/json/deserializer/EnumJsonDeserializer.java
+++ sharp-common/src/main/java/com/rick/common/http/json/deserializer/EnumJsonDeserializer.java
@@ -5,64 +5,20 @@ import com.fasterxml.jackson.core.JsonStreamContext;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.rick.common.util.ClassUtils;
 import com.rick.common.util.EnumUtils;
+import com.rick.common.util.JsonUtils;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.Map;
+
+import static com.fasterxml.jackson.databind.node.JsonNodeType.OBJECT;
+import static com.fasterxml.jackson.databind.node.JsonNodeType.STRING;
 
 /**
- * 自定义枚举解析器替换默认的枚举：枚举要遵循固定的格式：
- * <p>
- *     <b>1. name作为code</b>
- *     <blockquote><pre>
- *    {@literal @}AllArgsConstructor
- *    {@literal @}Getter
- *     public enum TypeEnum {
- *         PRIVATE("私立"),
- *         PUBLIC("公立");
- *
- *        {@literal @}JsonValue
- *         public String getCode() {
- *             return this.name();
- *         }
- *         private final String label;
- *         public static TypeEnum valueOfCode(String code) {
- *             return valueOf(code);
- *         }
- *     }</pre></blockquote>
- *     <b>2. 自定义数字code</b>
- *     <blockquote><pre>
- *    {@literal @}AllArgsConstructor
- *    {@literal @}Getter
- *     public enum SexEnum {
- *         UNKNOWN(0, "Unknown"),
- *         MALE(1, "Male"),
- *         FEMALE(2, "Female");
- *         private static final Map<Integer, SexEnum> codeMap = new HashMap<>();
- *         static {
- *             for (SexEnum e : values()) {
- *                 codeMap.put(e.code, e);
- *             }
- *         }
- *         private final int code;
- *         private final String label;
- *        {@literal @}JsonValue
- *         public int getCode() {
- *             return this.code;
- *         }
- *         public static SexEnum valueOfCode(int code) {
- *             return codeMap.get(code);
- *         }
- *     }
- *     </pre></blockquote>
- *
- * </p>
- * @author Rick
- * @createdAt 2021-10-11 21:56:00
- * EnumDeserializer 自带的完成相同的功能，按照顺序依次解析：
- *  如果有@JsonValue：先根据value，没有再根据order，没有报错
- *  如果没有@JsonValue：先根据name，没有再根据order，没有报错
+ * EnumJsonDeserializer 已经被 EnumCustomizeDeserializer 替换，这个类在多层结构下，枚举会出现问题
  */
 @Deprecated
 public class EnumJsonDeserializer extends JsonDeserializer<Enum> {
@@ -70,19 +26,40 @@ public class EnumJsonDeserializer extends JsonDeserializer<Enum> {
     @Override
     public Enum deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+
+        JsonNodeType nodeType = node.getNodeType();
+
         String code = node.asText();
+        if (nodeType == OBJECT) {
+            code = (String) JsonUtils.toObject(node.asOptional().get(), Map.class).get("code");
+        } else if (nodeType == STRING) {
+            code = node.asText();
+        }
 
         JsonStreamContext parsingContext = jsonParser.getParsingContext();
-        Object currentValue = parsingContext.getCurrentValue();
-        // 字段名
-        String currentName = parsingContext.getCurrentName();
 
         Field field = null;
         Enum value = null;
         try {
-            field = ClassUtils.getField(currentValue.getClass(),  currentName);
-            value = EnumUtils.valueOfCode(field.getType(), code);
-        } catch (Exception e) {}
+            if (parsingContext.hasCurrentName()) {
+                Object currentValue = parsingContext.getCurrentValue();
+                // 字段名
+                String currentName = parsingContext.getCurrentName();
+
+                field = ClassUtils.getField(currentValue.getClass(), currentName);
+                value = EnumUtils.valueOfCode(field.getType(), code);
+            } else if (parsingContext.getParent().hasCurrentName()) {
+                Object currentValue = jsonParser.getParsingContext().getParent().getCurrentValue();
+                String currentName = jsonParser.getParsingContext().getParent().getCurrentName();
+                field = ClassUtils.getField(currentValue.getClass(), currentName);
+                Class<?>[] fieldGenericClass = ClassUtils.getFieldGenericClass(field);
+                value = EnumUtils.valueOfCode(fieldGenericClass[0], code);
+            }
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException(
+                    "No enum constant " + field.getName() + "." + code);
+        }
 
         if (value == null) {
             throw new IllegalArgumentException(
