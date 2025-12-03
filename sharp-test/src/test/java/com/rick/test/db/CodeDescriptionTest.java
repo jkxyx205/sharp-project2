a59b2a02955d5dad1fbbe87cf79a1e8b5c78cc06
rick <jkxyx205@163.com>
2025-12-03 11:35:37 +0800

    chore: 更新 code

--- sharp-test/src/test/java/com/rick/test/db/CodeDescriptionTest.java
+++ sharp-test/src/test/java/com/rick/test/db/CodeDescriptionTest.java
@@ -26,6 +26,7 @@ public class CodeDescriptionTest {
     public void testSave() {
         codeDescriptionService.saveAll(CodeDescription.CategoryEnum.MATERIAL, Arrays.asList(
                 CodeDescription.builder().code("0001").description("物料组1").sort(0).build(),
+                CodeDescription.builder().code("0002").description("物料组1").sort(0).build(),
                 CodeDescription.builder().code("M2").description("物料组2").sort(1).build(),
                 CodeDescription.builder().code("M3").description("物料组3").sort(2).build()
         ));
@@ -35,6 +36,7 @@ public class CodeDescriptionTest {
     public void testSave2() {
         codeDescriptionService.saveAll(CodeDescription.CategoryEnum.PURCHASING_ORG, Arrays.asList(
                 CodeDescription.builder().code("0001").description("采购组织1").sort(0).build(),
+                CodeDescription.builder().code("0002").description("采购组织2").sort(0).build(),
                 CodeDescription.builder().code("PG2").description("采购组织2").sort(1).build(),
                 CodeDescription.builder().code("P-G-9").description("采购组织9").sort(9).build()
         ));
@@ -44,6 +46,7 @@ public class CodeDescriptionTest {
     public void testSave3() {
         codeDescriptionService.saveAll(CodeDescription.CategoryEnum.PACKAGING, Arrays.asList(
                 CodeDescription.builder().code("0001").description("包装组1").sort(0).build(),
+                CodeDescription.builder().code("0002").description("包装组2").sort(0).build(),
                 CodeDescription.builder().code("PKG1").description("包装组1").sort(1).build()
         ));
     }
@@ -51,14 +54,16 @@ public class CodeDescriptionTest {
     @Test
     public void testSave4() {
         codeDescriptionService.saveAll(CodeDescription.CategoryEnum.DIVISION, Arrays.asList(
-                CodeDescription.builder().code("0001").description("商品组").sort(0).build()
+                CodeDescription.builder().code("0001").description("商品组1").sort(0).build(),
+                CodeDescription.builder().code("0002").description("商品组2").sort(0).build()
         ));
     }
 
     @Test
     public void testSave5() {
         codeDescriptionService.saveAll(CodeDescription.CategoryEnum.DIRECT_DISCOVERY, Arrays.asList(
-                CodeDescription.builder().code("0001").description("直发现场理由1").sort(0).build()
+                CodeDescription.builder().code("0001").description("直发现场理由1").sort(0).build(),
+                CodeDescription.builder().code("0002").description("直发现场理由2").sort(0).build()
         ));
     }
 
