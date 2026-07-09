a58955cedb21bb34533979cc69cae481a6d27fe8
rick <jkxyx205@163.com>
2026-07-09 10:39:29 +0800

    refactor: 代码优化

--- sharp-database/src/main/java/com/rick/db/repository/OneToMany.java
+++ sharp-database/src/main/java/com/rick/db/repository/OneToMany.java
@@ -15,14 +15,16 @@ public @interface OneToMany {
     boolean cascadeSave() default true;
 
     /**
-     * 当 cascadeSave = true 的时候，是否执行删除操作， cascadeSaveNotDelete 只会在 cascadeSave = true 生效
+     * 当 cascadeSave = true 的时候，是否执行删除操作
+     * cascadeSaveItemDelete 只会在 cascadeSave = true 或 cascadeDelete = true 生效
      * @return
      */
     boolean cascadeSaveItemDelete() default true;
 
     /**
      * 删除前检查callback
-     * 当 cascadeSaveItemDelete = true 的时候，是否执行删除前检查操作， cascadeSaveItemDeleteCheck 只会在 cascadeSaveItemDelete = true 生效
+     * 当 cascadeSaveItemDelete = true 的时候，是否执行删除前检查操作
+     * cascadeSaveItemDeleteCheck 只会在 cascadeSaveItemDelete = true 或 cascadeDelete = true 生效 生效
      * @return
      */
     boolean cascadeSaveItemDeleteCheck() default false;
