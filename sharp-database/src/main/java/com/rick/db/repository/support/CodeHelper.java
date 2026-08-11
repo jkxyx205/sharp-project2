5350ecc02bb2083b09c58f2defa9e7f766cf043b
rick <jkxyx205@163.com>
2026-08-11 19:33:38 +0800

    feat: 组合 code 生成工具类

---
+++ sharp-database/src/main/java/com/rick/db/repository/support/CodeHelper.java
@@ -0,0 +1,41 @@
+package com.rick.db.repository.support;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Arrays;
+
+@UtilityClass
+public class CodeHelper {
+
+    String DEFAULT_DELIMITER = ":";
+
+    public String join(String... values) {
+        return join(DEFAULT_DELIMITER, values);
+    }
+
+    public String join(CharSequence delimiter, String... values) {
+        return String.join(delimiter, Arrays.stream(values)
+                .map(value -> value == null ? "" : value)
+                .toList());
+    }
+
+    public String[] split(String value) {
+        return split(value, DEFAULT_DELIMITER);
+    }
+
+    public String[] split(String value, CharSequence delimiter) {
+        if (value == null) {
+            return new String[0];
+        }
+
+        String[] values = value.split(String.valueOf(delimiter), -1);
+
+        for (int i = 0; i < values.length; i++) {
+            if (values[i].isEmpty()) {
+                values[i] = null;
+            }
+        }
+
+        return values;
+    }
+}
